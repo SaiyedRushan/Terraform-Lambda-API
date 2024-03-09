ebc5bf4d367f81d2f898e2d96bbc5529d472b74a
SaiyedRushan <rushan52@gmail.com>
2024-03-09 13:13:16 -0500

    update build script

--- lambda/build.sh
+++ lambda/build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # Exit if any of the following commands exit with non-0, and echo our commands back
-set -ex
-
-# For running npm "binaries"
-PATH=$PATH:./node_modules/.bin
-
-# Check that we're running the correct version of node
-check-node-version --package
+# set -ex
 
 # Compile TypeScript into "temp" (defined is tsconfig.json)
-tsc
+npx tsc
 
 # Install production dependencies under "temp"
 cp package*.json temp
@@ -19,5 +13,5 @@ cp package*.json temp
 # Create Lambda zipfile under "dist"
 (cd temp && zip -r ../dist/lambda.zip *)
 
-# Clean up
+# # Clean up
 rm -rf temp
